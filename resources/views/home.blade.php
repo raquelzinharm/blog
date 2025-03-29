91c1ddb66f70fb3d4b37651773bae48ad3ee6f41
raquelzinharm <raquelrmelo01@gmail.com>
2025-03-28 21:41:02 -0300

    categorias

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('wassup boyzzz') }}
                 </div>
             </div>
         </div>
